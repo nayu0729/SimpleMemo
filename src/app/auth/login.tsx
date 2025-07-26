4f2a8e8456d1cf93c7870a39f435c713664ba9d2
nayu0729 <146067630+nayu0729@users.noreply.github.com>
2025-07-26 18:44:32 +0900

    add login format

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -1,22 +1,41 @@
 import { JSX } from 'react'
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/Button'
 
 const handlePress = (): void => {
     router.replace('/memo/list')
-    
 }
 
 const LogIn = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input} value="Email address" />
-                <TextInput style={styles.input} value="Password" />
-                <Button label="Submit" onPress={handlePress}/>
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="Email Address"
+                    textContentType="emailAddress"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    autoCapitalize="none"
+                    secureTextEntry
+                    placeholder="Password"
+                    textContentType="password"
+                />
+                <Button label="Submit" onPress={handlePress} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registered?</Text>
                     <Link href="/auth/signUp" asChild replace>
