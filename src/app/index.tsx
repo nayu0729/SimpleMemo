432a81d3d779601fbe843f91c109848f5c1f2902
nayu0729 <146067630+nayu0729@users.noreply.github.com>
2025-07-26 16:08:17 +0900

    add login and memo pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,59 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Redirect } from 'expo-router'
 
 const Index = () => {
-    return (
-        <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>SimpleMemo</Text>
-                    <Text>ログアウト</Text>
-                </View>
-            </View>
-            <View>
-                <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2025年7月25日 20:38</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2025年7月25日 20:38</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2025年7月25日 20:38</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-            </View>
-
-            <View>
-                <Text>+</Text>
-            </View>
-        </View>
-    )
+    return <Redirect href="/auth/login" />
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
-
 export default Index
+
